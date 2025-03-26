e705261663909ef8df817db23eb741c41b7b3639
planexia-sa <planexia.sa@gmail.com>
2025-03-26 20:23:59 -0300

    vite3333333

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: 'portfolio-react',
+  base: '/portfolio-react/',  // Asegúrate de poner la barra al principio
   build: {
     outDir: 'dist',  // Asegúrate de que el directorio de salida sea correcto
   },
+  plugins: [react()],
 })
