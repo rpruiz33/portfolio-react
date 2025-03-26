bdabe19c8c1289623c3addf7493212ec5452bc25
planexia-sa <planexia.sa@gmail.com>
2025-03-26 20:20:03 -0300

    DEPLOY

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: 'portfolio-react',
-  plugins: [react()],
+  build: {
+    outDir: 'dist',  // Asegúrate de que el directorio de salida sea correcto
+  },
 })
