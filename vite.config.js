c31c117568becf82c645672d3c4c8c0557a10196
rpruiz33 <rpruiz33@gmail.com>
2025-03-27 10:09:32 -0300

    anda

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-export default defineConfig({
-  base: '/portfolio-react/',  // Asegúrate de poner la barra al principio
-  build: {
-    outDir: 'dist',  // Asegúrate de que el directorio de salida sea correcto
-  },
-  plugins: [react()],
-})
